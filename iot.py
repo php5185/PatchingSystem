08de8de9edc2128f87e84c1b1a8ee59251b4375f
Leonardo Abreu <leonardoeap@gmail.com>
2020-10-22 13:49:24 -0400

    Adding more progress

--- iot.py
+++ iot.py
@@ -1,39 +1,53 @@
 import random
+import time
 
 class iot:
+    def __init__(self):
+        self.lowMode = False
 
-    def __init__(self, data):
-        self.data = data
-        #self.process()
+        self.getRawData()
 
+    def activateLowMode(self):
+        self.lowMode = True
 
-    def sendData(self):
-        x=5
+    def sendData(self, data):
+        # x=5
+        print('Send Data:' + str(data))
         #sends data to node -- using Queue
 
     def getRawData(self):
-        #self.data = random.randrange(0, 10)
-        x=5
+        while(True):
+            time.sleep(2)
+            rowData = random.randrange(0, 10)
+
+            if (self.lowMode):
+                self.sendData(rowData)
+            else:
+                self.process(rowData)
 
     def patching(self, function):
         x=5
 
-    def process(self, function='def printFoo():  print(\'example\')'):
-        # exec(function)
-        # printFoo()
-        x=5
+    # pylint: disable=E0202
+    def process(self, data):
+        # print('Im the core process')
+        newData = data * -1
+        print('Process:' + str(newData))
 
-    # if __name__ == '__main__':
-    #     exec('def printFoo():  print(\'foo\')')
-    #     printFoo()
+        self.sendData(newData)
+        # print(data)
 
-    def process2(self):
-        x=5
+    def update(self, function='def newProcess(): print(\'Im the new core process\')'):
+        context = {}
+        exec(function, context)
+
+        self.process = context['newProcess']
 
 class node:
+    def __init__(self, iotDevice):
+        # self.iotDevice = iotDevice
+        iotDevice.update()
 
-    def __init__(self):
-        x=5
 
     def processData(self):
         x=5
@@ -41,18 +55,13 @@ class node:
     def simulateIot(self):
         x=5
 
-
-
-# if __name__ == '__main__':
-#     device = iot(50)
-#     #device.process()
-
 if __name__ == '__main__':
+    device = iot()
 
-    exec('def printFoo():  print(\'foo\')')
-    printFoo()
+    # TODO: Add Queues
+    # device.update()
 
-    # device = iot(50)
+    # time.sleep(5)
+
+    # print('here')
     # device.process()
-
-

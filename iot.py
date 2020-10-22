38ede0bd73afe64638fe57c723b271d1bfd8c09e
php5185 <php5185@rit.edu>
2020-10-21 21:27:18 -0400

    init files

---
+++ iot.py
@@ -0,0 +1,58 @@
+import random
+
+class iot:
+
+    def __init__(self, data):
+        self.data = data
+        #self.process()
+
+
+    def sendData(self):
+        x=5
+        #sends data to node -- using Queue
+
+    def getRawData(self):
+        #self.data = random.randrange(0, 10)
+        x=5
+
+    def patching(self, function):
+        x=5
+
+    def process(self, function='def printFoo():  print(\'example\')'):
+        # exec(function)
+        # printFoo()
+        x=5
+
+    # if __name__ == '__main__':
+    #     exec('def printFoo():  print(\'foo\')')
+    #     printFoo()
+
+    def process2(self):
+        x=5
+
+class node:
+
+    def __init__(self):
+        x=5
+
+    def processData(self):
+        x=5
+
+    def simulateIot(self):
+        x=5
+
+
+
+# if __name__ == '__main__':
+#     device = iot(50)
+#     #device.process()
+
+if __name__ == '__main__':
+
+    exec('def printFoo():  print(\'foo\')')
+    printFoo()
+
+    # device = iot(50)
+    # device.process()
+
+

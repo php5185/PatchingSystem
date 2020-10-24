0b73c05781af42c152bb7f287ae0a9d957a246c9
Saad Khan <sk6786@g.rit.edu>
2020-10-24 19:03:36 -0400

    finished v1

--- iot.py
+++ iot.py
@@ -3,11 +3,13 @@ import time
 from multiprocessing import Process, Queue
 
 class iot:
-    def __init__(self, iotDataQueue, iotStatusQueue):
+    def __init__(self, iotProcessDataQueue, iotRawDataQueue, iotStatusQueue):
         self.lowMode = False
         self.patchingProgress = 0
-        self.iotDataQueue = iotDataQueue
+        self.iotProcessDataQueue = iotProcessDataQueue
+        self.iotRawDataQueue = iotRawDataQueue
         self.iotStatusQueue = iotStatusQueue
+        self.version = 'v1'
 
     def listen_queue(self, queue):
         while True:
@@ -28,10 +30,16 @@ class iot:
     def activateLowMode(self):
         self.lowMode = True
 
-    def sendData(self, data):
+    def sendProcessData(self, data):
         # x=5
-        print('Send Data:' + str(data))
-        self.iotDataQueue.put({'data': data})
+        print('Send Process Data (' + self.version + '):' + str(data))
+        self.iotProcessDataQueue.put({'data': data})
+        #sends data to node -- using Queue
+
+    def sendRawData(self, data):
+        # x=5
+        print('Send Raw Data:' + str(data))
+        self.iotRawDataQueue.put({'data': data})
         #sends data to node -- using Queue
 
     def patch_progress(self):
@@ -40,13 +48,14 @@ class iot:
             self.lowMode = False
             self.iotStatusQueue.put({'lowMode': self.lowMode })
             self.patchingProgress = 0
+            self.version = 'v2'
 
     def getRawData(self):
         rawData = random.randrange(0, 10)
         if self.lowMode:
             self.patch_progress()
             print('iot in low mode')
-            self.sendData(rawData)
+            self.sendRawData(rawData)
         else:
             print('iot in regular mode')
             self.process(rawData)
@@ -59,8 +68,7 @@ class iot:
         # print('Im the core process')
         newData = data * -1
         print('Process:' + str(newData))
-
-        self.sendData(newData)
+        self.sendProcessData(newData)
         # print(data)
 
     def update(self, function):
@@ -69,8 +77,8 @@ class iot:
         setattr(self.__class__, 'process', context['process'])
 
 class node:
-    def __init__(self, iotDataQueue, iotStatusQueue, iotDevice):
-        self.iotDataQueue = iotDataQueue
+    def __init__(self, iotRawDataQueue, iotStatusQueue, iotDevice):
+        self.iotRawDataQueue = iotRawDataQueue
         self.iotStatusQueue = iotStatusQueue
         self.lowMode = False
         self.iotDevice = iotDevice
@@ -84,8 +92,8 @@ class node:
                     self.lowMode = mode['lowMode']
 
             if self.lowMode:
-                if not self.iotDataQueue.empty():
-                    data = self.iotDataQueue.get()['data']
+                if not self.iotRawDataQueue.empty():
+                    data = self.iotRawDataQueue.get()['data']
                     print('NODE PROCESS: ', data)
                     self.processData(data)
 
@@ -95,29 +103,41 @@ class node:
     def simulateIot(self):
         x=5
 
+class system:
+    def __init__(self, patchQueue,iotProcessDataQueue):
+        self.patchQueue = patchQueue
+        self.iotProcessDataQueue = iotProcessDataQueue
+
+    def listen_process_queue(self):
+        while True:
+            time.sleep(1)
+            if not self.iotProcessDataQueue.empty():
+                process_data = self.iotProcessDataQueue.get()
+                if 'data' in process_data:
+                    print('System receives: ' + str(process_data['data']))
+
+    def send_patch(self, patch):
+        time.sleep(5)
+        print('sending patch')
+        self.patchQueue.put({'patch': patch})
+
+
 if __name__ == '__main__':
-    iotDataQueue = Queue()
+    iotRawDataQueue = Queue()
+    iotProcessDataQueue = Queue()
     iotStatusQueue = Queue()
-
-    device = iot(iotDataQueue, iotStatusQueue)
-    nodeDevice = node(iotDataQueue, iotStatusQueue, device)
-
     patchQueue = Queue()
+
+    patching_system = system(patchQueue, iotProcessDataQueue)
+    device = iot(iotProcessDataQueue, iotRawDataQueue, iotStatusQueue)
+    nodeDevice = node(iotRawDataQueue, iotStatusQueue, device)
+    system_process = Process(target=patching_system.listen_process_queue, args=())
+    system_process.start()
     iot_process = Process(target=device.listen_queue, args=(patchQueue,))
     iot_process.start()
     node_process = Process(target=nodeDevice.listen_data_queue, args=())
     node_process.start()
-    time.sleep(4)
-    patchQueue.put({'patch': 'def process(self,data):\n\tself.sendData(data-100)'})
-
-
+    patching_system.send_patch('def process(self,data):\n\tself.sendProcessData(data-100)')
     iot_process.join()
     node_process.join()
-
-
-    # TODO: Add Queues
-    # device.update()
-
-
-    # print('here')
-    # device.process()
+    system_process.join()
